990c710c421f3980178685ded94ff6fe89a70c04
ghatolamol <ahghatol@gmail.com>
2019-05-18 16:08:38 +0530

    Article list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http'; 
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 
 @NgModule({
@@ -15,8 +17,10 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false ,
   imageCdn    : '//d3r1ne9mbvdkg1.cloudfront.net/',
-  apiBaseUrl  : '//app_staging.osianama.com/',
-  apiBase     : '//app_staging.osianama.com/',
+  apiBaseUrl  : '//smart-citizen.tekdi.net/',
+  apiBase     : '//smart-citizen.tekdi.net/',
   isProd : false,
 };
