fac18de07ab4f6b360fc600589f2f146de712729
ghatolamol <ahghatol@gmail.com>
2019-05-20 14:57:10 +0530

    Added login and logout link in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'projects/user-login/src/lib/providers/login.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userAction;
+
+  constructor(public login: LoginService, public route: Router) { }
 
   ngOnInit() {
+    this.userActionName();
   }
 
+  userActionName() {
+    this.userAction = this.login.isLoggedIn() ? 'Log Out' : 'Login & Signup';
+    console.log(this.userAction);
+  }
+
+  loginOrLogout() {
+    if (!this.login.isLoggedIn()) {
+      this.route.navigate(['/user']);
+    } else {
+      this.login.logout();
+    }
+
+    this.userActionName();
+  }
 }
